61280877599cb08fab78ccf6ac93903bff1fe636
imrane-ait-dahmade <imraneaitdahmade2006@gmail.com>
2024-12-31 13:17:43 +0100

    j ai faire une shema comme user case mais plus detail il existe en readme et j ai faire une page home pour le user non inconue

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # imrane_ait_dahmade-project
 Gestionnaire de Projets (OOP)
+
+La plateforme vise à faciliter la gestion et la collaboration des équipes en offrant aux utilisateurs un espace pour créer, suivre, et gérer leurs projets. 
+
+1) faire des shemas des actions de site comme user case mais plus detail :
+https://drive.google.com/file/d/1bVQSAwJwMxba2ZnjkZpvOSl77fwV6oix/view?usp=sharing
---
+++ view/page_home.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="bg-green-200 ">
+    <nav class="bg-white flex justify-center">
+        <ul class="nav text-green-700 gap-4 text-xl">
+            <li class="nav-item ">
+                <a class="nav-link active" aria-current="page" href="#">home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">about us</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">contact</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" >log in</a>
+            </li>
+        </ul>
+    </nav>
+    <div class=" flex flex-row ">
+    <div class="card w-1/3 pb-4 m-4 ">
+  <img src="https://chef-de-projet.fr/wp-content/uploads/2021/11/Planification-1280x640.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title text-2xl text-green-600">welecom to our Website</h5>
+    <p class="card-text text-xl">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary bg-green-200 mt-4 text-xl px-6 border-transparent">log-in</a>
+  </div>
+</div>
+<div>
+
+</div>
+</div>
+</body>
+
+</html>
